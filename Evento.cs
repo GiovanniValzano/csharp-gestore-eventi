5138fbae2cf840685702342f9efef77aaad7575b
GiovanniValzano <valzano.giovanni@gmail.com>
2023-03-27 11:52:38 +0200

    milestone 1-2

---
+++ Evento.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Evento
+    {
+        public Evento(string titolo, string data, string maxCapienza)
+        {
+            if (string.IsNullOrEmpty(titolo))
+            {
+                throw new Exception("Il titolo non può essere vuoto.");
+            }
+            Titolo = titolo;
+            DateTime dataConvertita;
+            if (!DateTime.TryParse(data, out dataConvertita))
+            {
+                throw new Exception("La data non è stata inserita nel formato corretto (dd,MM,yyyy)");
+            }
+            if (dataConvertita < DateTime.Now)
+            {
+                throw new Exception("Non è possibile inserire una data passata.");
+            }
+            Data = dataConvertita;
+            int maxCapienzaCon;
+            if (!int.TryParse(maxCapienza, out maxCapienzaCon))
+            {
+                throw new Exception("Il numero dei posti non è stato inserito correttamente");
+            }
+            if (maxCapienzaCon < 0)
+            {
+                throw new Exception("Il nomero di posti disponibili deve essere maggiore di 0.");
+            }
+            MaxCapienza = maxCapienzaCon;
+            NumPrenotazioni = 0;
+        }
+
+        public string Titolo { get; set; }
+        public DateTime Data { get; set; }
+        public int MaxCapienza { get; init; }
+        public int NumPrenotazioni { get; private set; }
+
+        public void PrenotaPosti(int postiPrenotati)
+        {
+            NumPrenotazioni += postiPrenotati;
+        }
+
+        public void DisdiciPosti(int postiDisdetti)
+        {
+            NumPrenotazioni -= postiDisdetti;
+        }
+
+        public override string ToString()
+        {
+            var data = Data.ToString("d");
+            string res = $"{Data} - {Titolo}";
+            return res;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GestoreEventi;
+using System.Runtime.ExceptionServices;
+
+List<Evento> eventi = new List<Evento>();
+
+Console.WriteLine("Benvenuti sul portale Eventi.");
+Console.WriteLine("Per creare un nuovo evento è necessario inserire un titolo:");
+var titolo = Console.ReadLine();
+Console.WriteLine("Inserire la data dell'evento:");
+var data = Console.ReadLine();
+Console.WriteLine("Inserisci il numero di posti totali dell'evento:");
+var postiTot = Console.ReadLine();
+
+try
+{
+    var eventoSelezionato = new Evento(titolo, data, postiTot);
+    eventi.Add(eventoSelezionato);
+    Console.WriteLine("Evento inserito correttamente.");
+    Prenotazione(eventoSelezionato);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+void Prenotazione(Evento evento)
+{
+    Console.WriteLine();
+    Console.WriteLine("Ora è possibile preotare dei posti per questo evento. Desideri farlo? (si/NO)");
+    var siNo = Console.ReadLine();
+
+    if (siNo == "si")
+    {
+        Console.WriteLine("Indicare il numero di posti da voler prenotare:");
+        var postiPrenotati = Console.ReadLine();
+        if (int.TryParse(postiPrenotati, out int result))
+        {
+            if (result < evento.MaxCapienza)
+            {
+                evento.PrenotaPosti(result);
+            }
+            else
+            {
+                Console.WriteLine("Numero di posti non disponibile");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Valore inserito non valido.");
+        }
+    }
+}
